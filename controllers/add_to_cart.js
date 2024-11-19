fbad688a2435e7f7c14368a211e9ad7c6169fc0a
fazal <fzaal987645@gmail.com>
2024-11-19 14:56:03 +0500

    changes made new

--- controllers/add_to_cart.js
+++ controllers/add_to_cart.js
@@ -15,11 +15,28 @@ module.exports = {
         const {  buyer_unique_id ,  product_name, seller_name, product_price , warranty, product_image  } =  req.body;
 
         console.log('Buyer Unique ID:', buyer_unique_id);
-  console.log('Product Name:', product_name);
-  console.log('Seller Name:', seller_name);
-  console.log('Product Price:', product_price);
-  console.log('Warranty:', warranty);
-  console.log('Product Image:', product_image);
+        console.log('Product Name:', product_name);
+        console.log('Seller Name:', seller_name);
+        console.log('Product Price:', product_price);
+        console.log('Warranty:', warranty);
+        console.log('Product Image:', product_image);
+
+        try {
+            const user = await Users_import.add_to_cart(buyer_unique_id , product_name , seller_name ,product_price ,warranty ,product_image)
+            if(user)
+            {
+                return res.status(200).json({ user });
+            }
+            else {
+                
+                return res.status(400).json({ message: "Data couldnot be added" });
+
+            }
+        } catch (error) {
+            
+        }
+       
+
 
     }
 
